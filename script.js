3cbea1037b9f71ee7006ba98de66e5c01758769c
teli203 <teli203@aol.com>
2025-04-09 00:17:01 -0500

    Create js file for card stack functions

---
+++ script.js
@@ -0,0 +1,14 @@
+// Card Stack section for 3 top Hero's //
+  document.querySelectorAll(".cards").forEach(stack => {
+    stack.querySelectorAll("img").forEach(img => {
+      img.addEventListener("click", () => {
+        stack.appendChild(img); // Moves clicked img to the end //
+        // Reset z-index based on new order //
+        [...stack.children].forEach((img, i, arr) => {
+          img.style.zIndex = arr.length - i;
+          img.style.transform = `translate(${i * 5}px, ${i * 5}px)`;
+        });
+      });
+    });
+  });
+
