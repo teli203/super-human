302279bdf77070c27bc0ace8d293a92398727980
teli203 <teli203@aol.com>
2025-04-14 21:28:04 -0500

    Create blog JS file with light and dark functions

---
+++ blog.js
@@ -0,0 +1,8 @@
+// Light & Dark Mode //
+
+const toggleBtn = document.getElementById('toggleMode');
+
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  toggleBtn.textContent = document.body.classList.contains('dark') ? '☀️' : '🌙';
+});
